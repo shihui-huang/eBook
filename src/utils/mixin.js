d7fb5d415f8087d5f2f0ddb724a2fa05f52d67cb
Paul Mabileau <paulmabileau@hotmail.fr>
2021-03-31 16:48:40 +0200

    Slight reformat
    Fix indentation.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -38,32 +38,32 @@ export const ebookMixin = {
       'setMetadata',
     ]),
     initGlobalStyle(defaultTheme) {
-  removeAllCss()
-  switch (defaultTheme) {
-    case 'Default':
-      addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_default.css`)
-      break
-    case 'Eye':
-      addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_eye.css`)
-      break
-    case 'Gold':
-      addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_gold.css`)
-      break
-    case 'Night':
-      addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_night.css`)
-      break
-    default:
-      addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_default.css`)
-  }
+      removeAllCss()
+      switch (defaultTheme) {
+        case 'Default':
+          addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_default.css`)
+          break
+        case 'Eye':
+          addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_eye.css`)
+          break
+        case 'Gold':
+          addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_gold.css`)
+          break
+        case 'Night':
+          addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_night.css`)
+          break
+        default:
+          addCss(`${process.env.VUE_APP_RES_URL}/theme/theme_default.css`)
+      }
     },
     refreshLocation() {
       const currentLocation = this.currentBook.rendition.currentLocation()
       if (this.currentLocation && this.currentLocation.start) {
-      const startCfi = currentLocation.start.cfi
-      const progress = this.currentBook.locations.percentageFromCfi(startCfi)
-      this.setProgress(Math.floor(progress * 100))
-      this.setChapter(currentLocation.start.index)
-      saveLocation(this.fileName, startCfi)
+        const startCfi = currentLocation.start.cfi
+        const progress = this.currentBook.locations.percentageFromCfi(startCfi)
+        this.setProgress(Math.floor(progress * 100))
+        this.setChapter(currentLocation.start.index)
+        saveLocation(this.fileName, startCfi)
       }
     },
     displayProgress() {
